839a5e1b7e0474c181574ef6349d177dc183ad61
Getto2asher <egorstasyk@gmail.com>
2024-03-02 00:50:20 +0700

    3 commit

--- 2.py
+++ 2.py
@@ -1,5 +1,5 @@
 time = int(input())
 hour = time // 3600
-minute = (time%3600)//60
-second = (time%3600)%60
-print('Текущее время: ', hour,'-',minute,'-', second)
+minute = (time % 3600) // 60
+second = (time % 3600) % 60
+print("Текущее время: ", hour, "-", minute, "-", second)
